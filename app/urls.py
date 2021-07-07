2b0c282a815f7c64c170916005f4b36bf992cc0e
Mayank Singh <85062204+mayanksingh2233@users.noreply.github.com>
2021-07-07 22:49:05 +0530

    Add files via upload

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from app import views
+urlpatterns = [
+    path("",views.index,name="home"),
+    path("login",views.loginuser,name="login"),
+    path("logout",views.logoutuser,name="logout")
+
+]
+    
